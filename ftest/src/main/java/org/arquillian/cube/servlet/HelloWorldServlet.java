a14decb7f5b6ec49c48b48c137981d7de30c5e44
Aslak Knutsen <aslak@4fs.no>
2014-11-09 00:54:21 +0100

    Only start dockerContainers that match active Arquillian qualifiers
    Match dockerContainer images against Containers in the ContainerRegistry.
    
    fixes #3

--- ftest/src/main/java/org/arquillian/cube/servlet/HelloWorldServlet.java
+++ ftest/src/main/java/org/arquillian/cube/servlet/HelloWorldServlet.java
@@ -12,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HelloWorld")
 public class HelloWorldServlet extends HttpServlet {
 
+    private static final long serialVersionUID = 1L;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         
