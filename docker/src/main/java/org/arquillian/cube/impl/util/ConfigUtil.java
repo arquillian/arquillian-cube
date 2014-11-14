bbcb3c489a7c9b78f79cd133ebc249b39d1fa85c
Aslak Knutsen <aslak@4fs.no>
2014-11-14 13:37:53 +0100

    Add simple 'extends' option to configuration
    Allow a Docker Container configuration to extend another.
    All top level config elements from the extended container
    will be copied over to the new configuration unless the new
    configuration has defined them.
    
        tomcat:
          image: x
          exposedPorts: [8080/tcp]
        tomcat2:
          extends: tomcat
          image: y
    
    tomcat2 will have the same exposedPorts as tomcat, but a different image.
    
    fixes #32

--- docker/src/main/java/org/arquillian/cube/impl/util/ConfigUtil.java
+++ docker/src/main/java/org/arquillian/cube/impl/util/ConfigUtil.java
@@ -2,6 +2,7 @@ package org.arquillian.cube.impl.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public final class ConfigUtil {
 
@@ -25,5 +26,26 @@ public final class ConfigUtil {
             result[n--] = cubeIds[i];
         }
         return result;
+    }
+
+    public static Map<String, Object> applyExtendsRules(Map<String, Object> containerConfig) {
+        for(Map.Entry<String, Object> containerEntry : containerConfig.entrySet()) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> container = (Map<String, Object>)containerEntry.getValue();
+            if(container.containsKey("extends")) {
+                String extendsContainer = container.get("extends").toString();
+                if(!containerConfig.containsKey(extendsContainer)) {
+                    throw new IllegalArgumentException(containerEntry.getKey() + " extends a non existing container definition " + extendsContainer);
+                }
+                @SuppressWarnings("unchecked")
+                Map<String, Object> extendedContainer = (Map<String, Object>)containerConfig.get(extendsContainer);
+                for(Map.Entry<String, Object> extendedContainerEntry : extendedContainer.entrySet()) {
+                    if(!container.containsKey(extendedContainerEntry.getKey())) {
+                        container.put(extendedContainerEntry.getKey(), extendedContainerEntry.getValue());
+                    }
+                }
+            }
+        }
+        return containerConfig;
     }
 }
