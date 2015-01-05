ce233553904af56155369b9250336d5ba281fedf
Alex Soto <asotobu@gmail.com>
2015-01-05 13:02:56 +0100

    adds business code for the test.

---
+++ containerless/src/test/java/org/arquillian/cube/impl/containerless/DaytimeServer.java
@@ -0,0 +1,31 @@
+package org.arquillian.cube.impl.containerless;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DaytimeServer {
+
+    public static void main(String[] args) {
+        
+        try (
+                ServerSocket serverSocket =
+                    new ServerSocket(8080);
+                Socket clientSocket = serverSocket.accept();     
+                PrintWriter out =
+                    new PrintWriter(clientSocket.getOutputStream(), true);                   
+            ) {
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+                out.write(simpleDateFormat.format(new Date()));
+                out.flush();
+            } catch (IOException e) {
+                System.out.println("Exception caught when trying to listen on port "
+                    + 8080 + " or listening for a connection");
+                System.out.println(e.getMessage());
+            }
+    }
+
+}
