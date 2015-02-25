dc3afa11056718320e7b85a84e1ecfb3af5c3fbf
Alex Soto <asotobu@gmail.com>
2015-02-25 13:18:00 +0100

    adds boot2docker ftest project.

---
+++ ftest-boot2docker/src/main/java/org/arquillian/cube/persistence/User.java
@@ -0,0 +1,30 @@
+package org.arquillian.cube.persistence;
+
+import java.util.UUID;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    protected User() {}
+
+    public User(String name) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
