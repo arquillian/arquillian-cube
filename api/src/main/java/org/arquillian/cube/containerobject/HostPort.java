8df54749f7f243802dd9f889c2ee5cd18d1280cf
Alex Soto <asotobu@gmail.com>
2015-11-20 11:08:26 +0100

    comments HostPort annotation.

--- api/src/main/java/org/arquillian/cube/containerobject/HostPort.java
+++ api/src/main/java/org/arquillian/cube/containerobject/HostPort.java
@@ -8,6 +8,19 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
+/**
+ * HostPort annotation is used to inject the bound port to a container object.
+ * Typically a docker image has an exposed port which is bound to a binding port (to access from outside).
+ * With this annotation you set the exposed port, and Cube will inject the bind port associated to this exposed port.
+ *
+ * For example given: 2222->22/tcp and using:
+ *
+ * @HostPort(22)
+ * int port;
+ *
+ * Port value will be 2222.
+ *
+ */
 @Target({FIELD})
 @Retention(RUNTIME)
 @Documented
