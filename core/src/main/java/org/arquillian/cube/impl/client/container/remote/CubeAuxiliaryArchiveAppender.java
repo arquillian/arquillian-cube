843c985701f8f15581c1079359f1cba7230e640c
Dipak Pawar <dpawar@redhat.com>
2017-01-09 17:44:04 +0530

    Adds requirements classes if tests running as in-container.

--- core/src/main/java/org/arquillian/cube/impl/client/container/remote/CubeAuxiliaryArchiveAppender.java
+++ core/src/main/java/org/arquillian/cube/impl/client/container/remote/CubeAuxiliaryArchiveAppender.java
@@ -2,8 +2,11 @@ package org.arquillian.cube.impl.client.container.remote;
 
 import org.arquillian.cube.CubeController;
 import org.arquillian.cube.impl.client.enricher.CubeControllerProvider;
+import org.arquillian.cube.requirement.ArquillianConditionalRunner;
+import org.arquillian.cube.spi.requirement.Requires;
 import org.jboss.arquillian.container.test.spi.RemoteLoadableExtension;
 import org.jboss.arquillian.container.test.spi.client.deployment.AuxiliaryArchiveAppender;
+import org.jboss.arquillian.core.spi.LoadableExtension;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
@@ -12,10 +15,16 @@ public class CubeAuxiliaryArchiveAppender implements AuxiliaryArchiveAppender {
 
     @Override
     public Archive<?> createAuxiliaryArchive() {
-        return ShrinkWrap.create(JavaArchive.class, "arquillian-cube.jar")
+        JavaArchive javaArchive = ShrinkWrap.create(JavaArchive.class, "arquillian-cube.jar")
                 .addPackage(CubeController.class.getPackage())
                 .addPackages(true, CubeRemoteExtension.class.getPackage())
                 .addClass(CubeControllerProvider.class)
                 .addAsServiceProvider(RemoteLoadableExtension.class, CubeRemoteExtension.class);
+
+        if (LoadableExtension.Validate.classExists("org.arquillian.cube.requirement.ArquillianConditionalRunner")) {
+            javaArchive.addPackages(true, ArquillianConditionalRunner.class.getPackage(), Requires.class.getPackage());
+        }
+
+        return javaArchive;
     }
 }
