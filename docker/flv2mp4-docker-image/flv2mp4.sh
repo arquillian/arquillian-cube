c6ac958c2341851faad653f0b69688021bb42d5e
hemanik <hemanikatyal@gmail.com>
2017-01-09 22:10:23 +0530

    Adds Dockerfile for flv-mp4 conversion.

--- docker/flv2mp4-docker-image/flv2mp4.sh
+++ docker/flv2mp4-docker-image/flv2mp4.sh
@@ -2,4 +2,10 @@
 
 cd /recording/
 for f in *.flv; do ffmpeg -i "$f" -crf 18 -preset ultrafast "${f%flv}mp4"; done
+<<<<<<< 995f396444bcb54a422c567f79e5e5d89d73fdff
 echo "CONVERSION COMPLETED"
+=======
+echo "CONVERSION COMPLETED"
+
+
+>>>>>>> Adds Dockerfile for flv-mp4 conversion.
