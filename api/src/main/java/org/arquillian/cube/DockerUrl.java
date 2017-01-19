1fe8b8958b8603fec3eccf8f537e570841e869ab
Alex Soto <asotobu@gmail.com>
2017-01-19 15:03:29 +0100

    adds @DockerUrl annotation enrichment in URLs (#556)
    * adds @DockerUrl annotation enrichment in URLs
    
    * removes unwanted annotation in stanalone test
    
    * change binding port and expose port to be different

---
+++ api/src/main/java/org/arquillian/cube/DockerUrl.java
@@ -0,0 +1,26 @@
+package org.arquillian.cube;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({FIELD, PARAMETER})
+@Retention(RUNTIME)
+@Documented
+public @interface DockerUrl {
+
+   /**
+    * Sets the cube name where you want to get the exposed port. Only useful in case of not using Container Object pattern.
+    * @return Cube Name
+    */
+   String containerName();
+   int exposedPort();
+
+   String protocol() default "http";
+   String context() default "/";
+
+}
