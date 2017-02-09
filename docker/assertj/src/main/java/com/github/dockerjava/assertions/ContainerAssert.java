e5408f241c1a8ae6333c87bfc89b7940972d67f4
Dipak Pawar <dpawar@redhat.com>
2017-02-09 16:29:18 +0530

    Adds hasProcessRunning implementation using rest api.

--- docker/assertj/src/main/java/com/github/dockerjava/assertions/ContainerAssert.java
+++ docker/assertj/src/main/java/com/github/dockerjava/assertions/ContainerAssert.java
@@ -7,12 +7,9 @@ import com.github.dockerjava.api.model.HostConfig;
 import org.assertj.core.api.AbstractAssert;
 import org.assertj.core.util.Objects;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -39,24 +36,6 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, InspectCont
       assertThat(getContainerConfig().getExposedPorts())
               .overridingErrorMessage("%nExpecting:%n <%s>%nto contain:%n <%s>", getContainerConfig().getExposedPorts(), Arrays.asList(ports))
               .contains(myports);
-
-      return this;
-   }
-
-   public ContainerAssert hasProcessRunning(String process) {
-      isNotNull();
-
-      InspectContainerResponse.ContainerState state = getContainerState();
-      ContainerStateAssert stateAssert = new ContainerStateAssert(state);
-      stateAssert.isNotNull();
-
-      this.isRunning();
-
-      final String actualProcessName = getProcessName(state.getPid());
-
-      assertThat(actualProcessName).isNotNull();
-      assertThat(actualProcessName).isNotEmpty();
-      assertThat(actualProcessName).isEqualToIgnoringCase(process);
 
       return this;
    }
@@ -195,24 +174,4 @@ public class ContainerAssert extends AbstractAssert<ContainerAssert, InspectCont
    private ContainerConfig getContainerConfig() {
       return this.actual.getConfig();
    }
-
-    private static String getProcessName(int processId) {
-
-        final String command = "ps -p " + processId + " -o comm=";
-        String output = "";
-        Process p;
-        try {
-            p = Runtime.getRuntime().exec(command);
-            p.waitFor();
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-            output = reader.lines().collect(Collectors.joining());
-
-        } catch (Exception e) {
-            throw new IllegalStateException("Unable to execute command" + command);
-        }
-
-        return output;
-    }
 }
--- docker/assertj/src/main/java/com/github/dockerjava/assertions/DockerJavaAssert.java
+++ docker/assertj/src/main/java/com/github/dockerjava/assertions/DockerJavaAssert.java
@@ -1,11 +1,15 @@
 package com.github.dockerjava.assertions;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.github.dockerjava.CubeOutput;
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.InspectContainerResponse;
 import com.github.dockerjava.api.model.Image;
+import com.github.dockerjava.core.command.ExecStartResultCallback;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Eddú Meléndez
@@ -31,6 +35,12 @@ public class DockerJavaAssert {
       return new ContainerAssert(container);
    }
 
+   public CubeOutputAssert withContainer(String name) {
+         String[] cmd = {"sh", "-c", "for i in $(ps -axo pid --no-headers); do echo `ps -p $i -o comm=`; done"};
+
+         return new CubeOutputAssert(getOutputOfCommand(name, cmd));
+   }
+
    public ContainersAssert containers(String... names) {
       List<InspectContainerResponse> containers = new ArrayList<InspectContainerResponse>();
       for (String containerName : names) {
@@ -47,4 +57,26 @@ public class DockerJavaAssert {
       return this.client.inspectContainerCmd(name).exec();
    }
 
+   private CubeOutput getOutputOfCommand(String containerId, String[] command) {
+      String execResponseId = execCreate(containerId, command);
+
+      ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+      OutputStream errorStream = new ByteArrayOutputStream();
+
+      try {
+         this.client.execStartCmd(execResponseId).withDetach(false)
+                 .exec(new ExecStartResultCallback(outputStream, errorStream)).awaitCompletion();
+      } catch (InterruptedException e) {
+         return new CubeOutput("", "");
+      }
+
+      return new CubeOutput(outputStream.toString().trim(), errorStream.toString().trim());
+   }
+
+   private String execCreate(String containerId, String... command) {
+      return this.client.execCreateCmd(containerId)
+              .withAttachStdout(true).withAttachStderr(true)
+              .withAttachStdin(true).withTty(false)
+              .withCmd(command).exec().getId();
+   }
 }
