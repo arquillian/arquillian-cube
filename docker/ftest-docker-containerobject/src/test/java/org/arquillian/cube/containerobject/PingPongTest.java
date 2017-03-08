fc120b6d0d2cbc4c89692a039061fb46ebdbbe12
Alex Soto <asotobu@gmail.com>
2017-03-08 12:18:48 +0100

    Adds test to reproduce problem

--- docker/ftest-docker-containerobject/src/test/java/org/arquillian/cube/containerobject/PingPongTest.java
+++ docker/ftest-docker-containerobject/src/test/java/org/arquillian/cube/containerobject/PingPongTest.java
@@ -1,5 +1,6 @@
 package org.arquillian.cube.containerobject;
 
+import org.arquillian.cube.CubeIp;
 import org.arquillian.cube.docker.impl.requirement.RequiresDockerMachine;
 import org.arquillian.cube.requirement.ArquillianConditionalRunner;
 import org.junit.Test;
@@ -13,6 +14,7 @@ import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
 @RunWith(ArquillianConditionalRunner.class)
@@ -22,6 +24,9 @@ public class PingPongTest {
     @Cube
     PingPongContainer pingPongContainer;
 
+    @CubeIp(containerName = "pingpong")
+    private String pingpongAddr;
+
     @Test
     public void shouldReturnOkAsPong() throws IOException {
         String pong = ping();
@@ -29,6 +34,10 @@ public class PingPongTest {
         assertThat(pingPongContainer.getConnectionPort(), is(5000));
     }
 
+    @Test
+    public void shouldInjectCubeIp() {
+        assertThat(pingpongAddr, notNullValue());
+    }
 
     public String ping() throws IOException {
         URL url = new URL("http://" + pingPongContainer.getDockerHost() + ":" + pingPongContainer.getConnectionPort());
