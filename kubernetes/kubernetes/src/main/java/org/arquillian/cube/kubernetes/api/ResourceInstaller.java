95e96047af00d5cde4570f58f424b1fecf2daf89
Ioannis Canellos <iocanel@gmail.com>
2017-03-09 14:35:37 +0200

    Ns cleanup destroy (#616)
    * Namespace cleanup now refers to the process of (removing the resources without deleting the namespace). Deleting the namespace is now called `namespace destroy`.
    
    * ResourceInstaller now implements WIthToImmutable.
    
    * Bump to kubernetes-client 2.2.1.

--- kubernetes/kubernetes/src/main/java/org/arquillian/cube/kubernetes/api/ResourceInstaller.java
+++ kubernetes/kubernetes/src/main/java/org/arquillian/cube/kubernetes/api/ResourceInstaller.java
@@ -2,10 +2,31 @@ package org.arquillian.cube.kubernetes.api;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Map;
 
 import io.fabric8.kubernetes.api.model.HasMetadata;
 
-public interface ResourceInstaller {
+public interface ResourceInstaller extends WithToImmutable<ResourceInstaller> {
 
+    /**
+     * Installs the resources found in the specified URL.
+     * @param url   The URL to read resources from.
+     * @return      The list with the created resources.
+     */
     List<HasMetadata> install(URL url);
+
+    /**
+     * Uninstalls the resources found in the specified URL.
+     * @param url   The URL to read resources from.
+     * @return      A map of the resources to their delete status.
+     */
+    Map<HasMetadata, Boolean> uninstall(URL url);
+
+
+    /**
+     * Uninstalls the resources found in the specified list.
+     * @param list      The list with the resources.
+     * @return          A map of the resources to their delete status.
+     */
+    Map<HasMetadata, Boolean> uninstall(List<HasMetadata> list);
 }
--- kubernetes/kubernetes/src/main/java/org/arquillian/cube/kubernetes/impl/install/DefaultResourceInstaller.java
+++ kubernetes/kubernetes/src/main/java/org/arquillian/cube/kubernetes/impl/install/DefaultResourceInstaller.java
@@ -1,6 +1,9 @@
 package org.arquillian.cube.kubernetes.impl.install;
 
+import org.arquillian.cube.kubernetes.api.Configuration;
+import org.arquillian.cube.kubernetes.api.Logger;
 import org.arquillian.cube.kubernetes.api.ResourceInstaller;
+import org.arquillian.cube.kubernetes.api.WithToImmutable;
 import org.arquillian.cube.kubernetes.impl.visitor.CompositeVisitor;
 import org.jboss.arquillian.core.api.Instance;
 import org.jboss.arquillian.core.api.annotation.Inject;
@@ -9,7 +12,10 @@ import org.jboss.arquillian.core.spi.ServiceLoader;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
 import io.fabric8.kubernetes.api.builder.Visitor;
 import io.fabric8.kubernetes.api.model.HasMetadata;
@@ -22,18 +28,127 @@ public class DefaultResourceInstaller implements ResourceInstaller {
     protected Instance<KubernetesClient> client;
 
     @Inject
+    protected Instance<Configuration> configuration;
+
+    @Inject
+    protected Instance<Logger> logger;
+
+    @Inject
     protected Instance<ServiceLoader> serviceLoader;
+
+    protected ResourceInstaller delegate;
+
+
+    @Override
+    public ResourceInstaller toImmutable() {
+        if (delegate != null) {
+            return delegate;
+        }
+        synchronized (this) {
+            if (delegate == null) {
+                delegate = new DefaultResourceInstaller.ImmutableResourceInstaller(client.get(), configuration.get(),
+                        logger.get().toImmutable(),
+                        new ArrayList<>(serviceLoader.get().all(Visitor.class)));
+            }
+        }
+        return delegate;
+    }
 
     @Override
     public List<HasMetadata> install(URL url) {
-        ServiceLoader serviceLoader = this.serviceLoader.get();
-        KubernetesClient client = this.client.get();
-        List<Visitor> visitors = new ArrayList<>(serviceLoader.all(Visitor.class));
-        CompositeVisitor compositeVisitor = new CompositeVisitor(visitors);
-        try (InputStream is = url.openStream()) {
-            return client.load(is).accept(compositeVisitor).createOrReplace();
-        } catch (Throwable t) {
-            throw KubernetesClientException.launderThrowable(t);
+        return toImmutable().install(url);
+    }
+
+    @Override
+    public Map<HasMetadata, Boolean> uninstall(URL url) {
+        return toImmutable().uninstall(url);
+    }
+
+    @Override
+    public Map<HasMetadata, Boolean> uninstall(List<HasMetadata> list) {
+        return toImmutable().uninstall(list);
+    }
+
+    public static class ImmutableResourceInstaller implements ResourceInstaller, WithToImmutable<ResourceInstaller> {
+
+
+        private final KubernetesClient client;
+        private final Configuration configuration;
+        private final Logger logger;
+        private final List<Visitor> visitors;
+
+
+        public ImmutableResourceInstaller(KubernetesClient client, Configuration configuration, Logger logger,  List<Visitor> visitors) {
+            this.client = client;
+            this.configuration = configuration;
+            this.logger = logger;
+            this.visitors = visitors;
+        }
+
+        @Override
+        public List<HasMetadata> install(URL url) {
+            CompositeVisitor compositeVisitor = new CompositeVisitor(visitors);
+            try (InputStream is = url.openStream()) {
+                return client.load(is).accept(compositeVisitor).createOrReplace();
+            } catch (Throwable t) {
+                throw KubernetesClientException.launderThrowable(t);
+            }
+        }
+
+        @Override
+        public Map<HasMetadata, Boolean> uninstall(URL url) {
+            Map<HasMetadata, Boolean> result = new HashMap<>();
+            CompositeVisitor compositeVisitor = new CompositeVisitor(visitors);
+
+            try (InputStream is = url.openStream()) {
+                return uninstall(client.load(is).accept(compositeVisitor).get());
+            } catch (Throwable t) {
+                throw KubernetesClientException.launderThrowable(t);
+            }
+        }
+
+        @Override
+        public Map<HasMetadata, Boolean> uninstall(List<HasMetadata> list) {
+            Map<HasMetadata, Boolean> result = new HashMap<>();
+            preUninstallCheck();
+            for (HasMetadata h : list) {
+                try {
+                    Boolean deleted = client.resource(h).delete();
+                    result.put(h, deleted);
+                } catch (Throwable t) {
+                    result.put(h, false);
+                }
+            }
+            return result;
+        }
+
+        public void preUninstallCheck() {
+            if (configuration.isNamespaceCleanupConfirmationEnabled()) {
+                logger.info("");
+                logger.info("Waiting to cleanup the namespace.");
+                logger.info("Please press <enter> to cleanup the namespace.");
+
+                Scanner scanner = new Scanner(System.in);
+                scanner.nextLine();
+                logger.info("Cleaning up...");
+                return;
+            } else {
+                long timeout = configuration.getNamespaceCleanupTimeout();
+                if (timeout > 0L) {
+                    logger.info("");
+                    logger.info("Waiting for " + timeout + " seconds before cleaning the namespace");
+                    try {
+                        Thread.sleep(timeout * 1000);
+                    } catch (InterruptedException e) {
+                        logger.info("Interrupted waiting to cleanup the namespace: " + e);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public ResourceInstaller toImmutable() {
+            return this;
         }
     }
 }
