65ef5041e58107b3c966a76c4376303c498546cf
James Netherton <jamesnetherton@users.noreply.github.com>
2017-04-04 14:48:15 +0100

    Fixes #660. Gracefully handle ExecutionException in DockerRequirement (#663)

--- docker/docker/src/main/java/org/arquillian/cube/docker/impl/requirement/DockerRequirement.java
+++ docker/docker/src/main/java/org/arquillian/cube/docker/impl/requirement/DockerRequirement.java
@@ -1,11 +1,13 @@
 package org.arquillian.cube.docker.impl.requirement;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.model.Version;
 import com.github.dockerjava.core.DockerClientBuilder;
 import com.google.common.base.Strings;
-import java.util.HashMap;
-import java.util.Map;
+
 import org.arquillian.cube.docker.impl.client.CubeDockerConfiguration;
 import org.arquillian.cube.docker.impl.client.CubeDockerConfigurationResolver;
 import org.arquillian.cube.docker.impl.util.Boot2Docker;
@@ -15,15 +17,26 @@ import org.arquillian.cube.docker.impl.util.OperatingSystemResolver;
 import org.arquillian.cube.docker.impl.util.Top;
 import org.arquillian.cube.spi.requirement.Requirement;
 import org.arquillian.cube.spi.requirement.UnsatisfiedRequirementException;
+import org.arquillian.spacelift.execution.ExecutionException;
 
 public class DockerRequirement implements Requirement<RequiresDocker> {
 
-    private final CommandLineExecutor commandLineExecutor = new CommandLineExecutor();
-    private final CubeDockerConfigurationResolver resolver = new CubeDockerConfigurationResolver(new Top(),
-        new DockerMachine(commandLineExecutor),
-        new Boot2Docker(commandLineExecutor),
-        new OperatingSystemResolver().currentOperatingSystem().getFamily()
-    );
+    private final CommandLineExecutor commandLineExecutor;
+    private final CubeDockerConfigurationResolver resolver;
+
+    public DockerRequirement() {
+        this.commandLineExecutor = new CommandLineExecutor();
+        this.resolver = new CubeDockerConfigurationResolver(new Top(),
+            new DockerMachine(commandLineExecutor),
+            new Boot2Docker(commandLineExecutor),
+            new OperatingSystemResolver().currentOperatingSystem().getFamily()
+        );
+    }
+
+    public DockerRequirement(CommandLineExecutor commandLineExecutor, CubeDockerConfigurationResolver resolver) {
+        this.commandLineExecutor = commandLineExecutor;
+        this.resolver = resolver;
+    }
 
     /**
      * @param serverUrl
@@ -52,12 +65,18 @@ public class DockerRequirement implements Requirement<RequiresDocker> {
 
     @Override
     public void check(RequiresDocker context) throws UnsatisfiedRequirementException {
-        Map<String, String> config = resolver.resolve(new HashMap<String, String>());
-        String serverUrl = config.get(CubeDockerConfiguration.DOCKER_URI);
-        if (Strings.isNullOrEmpty(serverUrl)) {
-            throw new UnsatisfiedRequirementException("Could not resolve the docker server url.");
-        } else if (!isDockerRunning(serverUrl)) {
-            throw new UnsatisfiedRequirementException("No server is running on url:[" + serverUrl + "].");
+        try {
+            Map<String, String> config = resolver.resolve(new HashMap<String, String>());
+            String serverUrl = config.get(CubeDockerConfiguration.DOCKER_URI);
+            if (Strings.isNullOrEmpty(serverUrl)) {
+                throw new UnsatisfiedRequirementException("Could not resolve the docker server url.");
+            } else if (!isDockerRunning(serverUrl)) {
+                throw new UnsatisfiedRequirementException("No server is running on url:[" + serverUrl + "].");
+            }
+        } catch (ExecutionException e) {
+            throw new UnsatisfiedRequirementException("Cannot execute docker command.");
         }
     }
+
+
 }
