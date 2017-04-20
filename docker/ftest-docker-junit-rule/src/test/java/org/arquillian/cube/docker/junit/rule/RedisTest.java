3459e9e1e413f8e6d5cd91361f6277dd38132674
Alex Soto <asotobu@gmail.com>
2017-04-20 07:02:07 +0200

    Adds conditional runner

--- docker/ftest-docker-junit-rule/src/test/java/org/arquillian/cube/docker/junit/rule/RedisTest.java
+++ docker/ftest-docker-junit-rule/src/test/java/org/arquillian/cube/docker/junit/rule/RedisTest.java
@@ -1,11 +1,16 @@
 package org.arquillian.cube.docker.junit.rule;
 
+import org.arquillian.cube.docker.impl.requirement.RequiresDockerMachine;
+import org.arquillian.cube.requirement.ArquillianConditionalRunner;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import redis.clients.jedis.Jedis;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(ArquillianConditionalRunner.class)
+@RequiresDockerMachine(name = "dev")
 public class RedisTest {
 
     @ClassRule
