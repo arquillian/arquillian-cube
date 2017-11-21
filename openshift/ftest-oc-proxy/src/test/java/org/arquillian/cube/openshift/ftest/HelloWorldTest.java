d09e3b1d2a3777b09afdeb68682de1c67d2aff87
Dipak Pawar <dpawar@redhat.com>
2017-11-21 18:49:20 +0530

    Adds changes as per review.

--- openshift/ftest-oc-proxy/src/test/java/org/arquillian/cube/openshift/ftest/HelloWorldTest.java
+++ openshift/ftest-oc-proxy/src/test/java/org/arquillian/cube/openshift/ftest/HelloWorldTest.java
@@ -1,35 +1,23 @@
 package org.arquillian.cube.openshift.ftest;
 
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 import org.arquillian.cube.kubernetes.impl.utils.CommandExecutor;
 import org.arquillian.cube.openshift.impl.requirement.RequiresOpenshift;
 import org.arquillian.cube.requirement.ArquillianConditionalRunner;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+// tag::client_cli_execution[]
 @RunWith(ArquillianConditionalRunner.class)
 @RequiresOpenshift
 public class HelloWorldTest {
 
-    private CommandExecutor commandExecutor;
-
-    @Before
-    public void initCommandExecutor() {
-        commandExecutor = new CommandExecutor();
-    }
-
-    @Test
-    public void should_be_able_deploy_resources_using_oc() {
-        // when
-        final List<String> resources = commandExecutor.execCommand("oc create -f https://goo.gl/qaiGRJ");
-
-        // then
-        assertThat(resources).contains("service \"hello-world\" created", "deployment \"hello-world\" created");
-    }
+    private static CommandExecutor commandExecutor = new CommandExecutor();
 
     @Test
     public void should_be_able_get_namespace_using_kubectl() {
@@ -40,10 +28,37 @@ public class HelloWorldTest {
         assertThat(namespaces).contains("namespaces/default");
     }
 
-    @After
-    public void deleteDeployment() {
-        final List<String> strings = commandExecutor.execCommand("oc delete -f https://goo.gl/qaiGRJ");
+    @Test
+    public void should_be_able_deploy_resources_using_oc() {
+        // given
+        String commandToExecute = "oc create -f " + getResource("openshift.json");
+
+        // when
+        final List<String> resources = commandExecutor.execCommand(commandToExecute);
+
+        // then
+        assertThat(resources).contains("service \"hello-world\" created", "deployment \"hello-world\" created");
+    }
+
+    @AfterClass
+    public static void deleteDeployment() {
+        String commandToExecute = "oc delete -f " + getResource("openshift.json");
+        final List<String> strings = commandExecutor.execCommand(commandToExecute);
 
         assertThat(strings).contains("service \"hello-world\" deleted", "deployment \"hello-world\" deleted");
     }
+
+    private static String getResource(String resourceName) {
+        final URL resource = Thread.currentThread().getContextClassLoader().getResource(resourceName);
+        if (resource == null) {
+            throw new IllegalStateException("Expected " + resourceName + " to be on the classpath");
+        }
+
+        try {
+            return resource.toURI().getPath();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+// end::client_cli_execution[]
