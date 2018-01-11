1907704ed36a6f630c509dcb76e0ff29c226a1e2
Dipak Pawar <dpawar@redhat.com>
2018-01-11 11:11:58 +0530

    updates after merge to make it runnable

--- openshift/ftest-openshift-restassured/src/test/java/org/arquillian/openshift/restassured/HelloOpenShiftRestAssuredIT.java
+++ openshift/ftest-openshift-restassured/src/test/java/org/arquillian/openshift/restassured/HelloOpenShiftRestAssuredIT.java
@@ -5,15 +5,17 @@ import org.arquillian.cube.openshift.impl.requirement.RequiresOpenshift;
 import org.arquillian.cube.requirement.ArquillianConditionalRunner;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
 
 // tag::openshift_restassured_example[]
+@Category(RequiresOpenshift.class)
 @RunWith(ArquillianConditionalRunner.class)
 @RequiresOpenshift
-public class HelloOpenShiftRestAssuredTest {
+public class HelloOpenShiftRestAssuredIT {
 
     @ArquillianResource
     RequestSpecBuilder requestSpecBuilder;
