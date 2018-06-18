c1c0a02060b23708a9c92a19a82db52f721d5f35
Dipak Pawar <dpawar@redhat.com>
2018-06-18 19:48:59 +0530

    chore: rename UserRepositoryTest to UserRepositoryIT

--- docker/ftest-jpa/src/test/java/org/arquillian/cube/persistence/UserRepositoryIT.java
+++ docker/ftest-jpa/src/test/java/org/arquillian/cube/persistence/UserRepositoryIT.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import javax.inject.Inject;
+import org.arquillian.cube.docker.impl.requirement.RequiresDocker;
+import org.arquillian.cube.docker.impl.requirement.RequiresDockerMachine;
+import org.arquillian.cube.requirement.ArquillianConditionalRunner;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
@@ -12,10 +15,12 @@ import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.resolver.api.maven.Maven;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-@RunWith(Arquillian.class)
-public class UserRepositoryTest {
+@Category({RequiresDocker.class})
+@RunWith(ArquillianConditionalRunner.class)
+public class UserRepositoryIT {
 
     @Inject
     private UserRepository repository;
@@ -30,7 +35,7 @@ public class UserRepositoryTest {
 
         return ShrinkWrap.create(WebArchive.class)
             .addAsLibraries(files)
-            .addClasses(User.class, UserRepository.class, UserRepositoryTest.class)
+            .addClasses(User.class, UserRepository.class, UserRepositoryIT.class)
             .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
             .addAsResource("test-persistence.xml", "META-INF/persistence.xml");
     }
