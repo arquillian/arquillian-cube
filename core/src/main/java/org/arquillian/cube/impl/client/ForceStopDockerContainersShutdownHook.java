0680c3a49c07d968287b653140469a220727cc4e
Andy Gumbrecht <agumbrecht@tomitribe.com>
2018-07-03 15:37:29 +0200

    fix: close all cubes, even on error (#1059)

--- core/src/main/java/org/arquillian/cube/impl/client/ForceStopDockerContainersShutdownHook.java
+++ core/src/main/java/org/arquillian/cube/impl/client/ForceStopDockerContainersShutdownHook.java
@@ -1,21 +1,35 @@
 package org.arquillian.cube.impl.client;
 
-import java.util.List;
 import org.arquillian.cube.spi.Cube;
 import org.arquillian.cube.spi.CubeRegistry;
 import org.jboss.arquillian.core.api.annotation.Observes;
 import org.jboss.arquillian.test.spi.event.suite.BeforeSuite;
 
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ForceStopDockerContainersShutdownHook {
 
     public void attachShutDownHookForceStopDockerContainers(@Observes(precedence = 200) BeforeSuite event,
-        final CubeRegistry cubeRegistry) {
+                                                            final CubeRegistry cubeRegistry) {
+
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             final List<Cube<?>> cubes = cubeRegistry.getCubes();
             for (Cube cube : cubes) {
 
+                try {
                     cube.stop();
-                    cube.destroy();
+                } catch (final Throwable e) {
+                    Logger.getLogger(ForceStopDockerContainersShutdownHook.class.getName()).log(Level.WARNING, "Failed to stop container: " + cube.getId(), e);
+                } finally {
+                    try {
+                        cube.destroy();
+                    } catch (final Throwable ignore) {
+                        //no-op
+                    }
+                }
+
             }
         }));
     }
