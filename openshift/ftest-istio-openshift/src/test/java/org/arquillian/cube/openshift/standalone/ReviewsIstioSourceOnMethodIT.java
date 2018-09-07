47333f430add7418f97026d8a524aed1e628f4f6
Georgios Andrianakis <geoand@gmail.com>
2018-09-07 14:41:52 +0300

    Enable setting IstioResource on test methods in addition to test class (#1083)

--- openshift/ftest-istio-openshift/src/test/java/org/arquillian/cube/openshift/standalone/ReviewsIstioSourceOnMethodIT.java
+++ openshift/ftest-istio-openshift/src/test/java/org/arquillian/cube/openshift/standalone/ReviewsIstioSourceOnMethodIT.java
@@ -1,6 +1,5 @@
 package org.arquillian.cube.openshift.standalone;
 
-import java.io.IOException;
 import java.net.URL;
 import org.arquillian.cube.istio.api.IstioResource;
 import org.arquillian.cube.istio.impl.IstioAssistant;
@@ -17,26 +16,20 @@ import org.junit.runner.RunWith;
 @RunWith(ArquillianConditionalRunner.class)
 @Category(RequiresOpenshift.class)
 @RequiresOpenshift
-@IstioResource("classpath:route-rule-reviews-test-v${serviceVersion:2}.yaml")
 @Ignore("This test assumes that you have a cluster installed with Istio and BookInfo application deployed. We could make a full test preparing all this, but it will take lot of time, not error safe and test execution would take like 10 minutes")
-public class ReviewsIT extends AbstractReviewsTest {
+public class ReviewsIstioSourceOnMethodIT extends AbstractReviewsTest {
 
     @RouteURL("productpage")
     @AwaitRoute
     private URL url;
 
-    // tag::istio_assistant[]
     @ArquillianResource
     private IstioAssistant istioAssistant;
-    // end::istio_assistant[]
 
+    @IstioResource("classpath:route-rule-reviews-test-v${serviceVersion:2}.yaml")
     @Test
-    public void should_get_v1_if_not_logged() {
-        should_get_v1_if_not_logged(url);
-    }
-
-    @Test
-    public void alex_should_use_reviews_v2_version() throws IOException {
+    public void alex_should_use_reviews_v2_version() throws Exception {
+        Thread.sleep(10000); //wait until the rule get's applied
         alex_should_use_reviews_v2_version(url, istioAssistant);
     }
 
