a9261611aa6ce0769cc60d21992cce93830fd845
Fabio Burzigotti <fburzigo@redhat.com>
2024-10-06 00:02:49 +0200

    [issues-1315] - Switch from javax.* to jakarta.* in Fabric8 maven plugin tests

--- fabric8-maven-plugin-build/src/test/resources/spring-boot-http-booster/src/main/java/io/openshift/booster/service/GreetingEndpoint.java
+++ fabric8-maven-plugin-build/src/test/resources/spring-boot-http-booster/src/main/java/io/openshift/booster/service/GreetingEndpoint.java
@@ -17,11 +17,11 @@ package io.openshift.booster.service;
 
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 
 @Path("/")
 @Component
