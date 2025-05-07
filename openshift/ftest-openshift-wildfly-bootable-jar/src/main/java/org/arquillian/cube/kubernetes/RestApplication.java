c7708f34f3bc319fb5cbb97939adb8e056db9b6d
Fabio Burzigotti <fburzigo@redhat.com>
2025-05-07 14:04:22 +0200

    [issue-1374] - Adding a WildFly test on OpenShift 4

---
+++ openshift/ftest-openshift-wildfly-bootable-jar/src/main/java/org/arquillian/cube/kubernetes/RestApplication.java
@@ -0,0 +1,8 @@
+package org.arquillian.cube.kubernetes;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
