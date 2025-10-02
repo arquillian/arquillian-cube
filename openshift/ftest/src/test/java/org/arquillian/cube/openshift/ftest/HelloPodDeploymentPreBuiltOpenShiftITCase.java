0574bdc6721554228a40b056fd01166fe2343204
striver-24 <deivyansh.iocl1@gmail.com>
2025-10-02 23:52:28 +0530

    Duplicated the test case and applied the new approach to validate the build from source approach, keeping the Git service use case separate.

--- openshift/ftest/src/test/java/org/arquillian/cube/openshift/ftest/HelloPodDeploymentPreBuiltOpenShiftITCase.java
+++ openshift/ftest/src/test/java/org/arquillian/cube/openshift/ftest/HelloPodDeploymentPreBuiltOpenShiftITCase.java
@@ -4,30 +4,29 @@ import java.net.URL;
 
 import org.arquillian.cube.olm.impl.requirement.RequiresOlm;
 import org.arquillian.cube.openshift.impl.requirement.RequiresOpenshift;
-import org.arquillian.cube.remote.requirement.RequiresRemoteResource;
 import org.arquillian.cube.requirement.ArquillianConditionalRunner;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.TargetsContainer;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-@Ignore("Needs the Gitea operator, which Arquillian Cube tries to provision, but it does not work on GitHub CI which uses CRC")
-@Category({RequiresOpenshift.class, RequiresOlm.class, RequiresRemoteResource.class})
+@Category({RequiresOpenshift.class, RequiresOlm.class})
 @RequiresOpenshift
 @RequiresOlm
 @RunWith(ArquillianConditionalRunner.class)
-public class HelloPodDeploymentOpenShiftITCase {
+public class HelloPodDeploymentPreBuiltOpenShiftITCase {
 
     @ArquillianResource
     private URL base;
 
     @Deployment(testable = false)
+    @TargetsContainer("hello-openshift-prebuilt")
     public static WebArchive deploy() {
         return ShrinkWrap.create(WebArchive.class)
             .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
